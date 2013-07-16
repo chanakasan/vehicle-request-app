7d5454c81ad13b949a31e07eb77d5c0423d19b1f
dream89 <chanakasan@gmail.com>
2013-07-16 12:31:14 +0530

    Add more RequestEmployee fixtures, Add [request, employee] uniqueConstraint

--- src/Panda86/AppBundle/DataFixtures/ORM/LoadRequestEmployeeData.php
+++ src/Panda86/AppBundle/DataFixtures/ORM/LoadRequestEmployeeData.php
@@ -25,35 +25,60 @@ class LoadRequestEmployeeData implements FixtureInterface
         $emp1 = $emp[0];
         $emp2 = $emp[1];
         $emp3 = $emp[2];
+        $emp4 = $emp[3];
+        $emp5 = $emp[4];
+        $emp6 = $emp[5];
+        $emp7 = $emp[6];
+        $emp8 = $emp[7];
+        $emp9 = $emp[8];
 
-        /* first record */
-        $reqEmp1 = new RequestEmployee();
-        $reqEmp1->setIsOwner(true);
-        $reqEmp1->setRequest($req1);
-        $reqEmp1->setEmployee($emp1);
+        /* first request */
+        // record one
+        $reqEmp11 = new RequestEmployee();
+        $reqEmp11->setIsOwner(true);
+        $reqEmp11->setRequest($req1);
+        $reqEmp11->setEmployee($emp1);
 
-        $manager->persist($req1);
-        $manager->persist($emp1);
-        $manager->persist($reqEmp1);
+        $manager->persist($reqEmp11);
 
-        /* second record */
-        $reqEmp2 = new RequestEmployee();
-        $reqEmp2->setRequest($req1);
-        $reqEmp2->setEmployee($emp2);
+        // record two
+        $reqEmp12 = new RequestEmployee();
+        $reqEmp12->setRequest($req1);
+        $reqEmp12->setEmployee($emp2);
 
-        $manager->persist($req1);
-        $manager->persist($emp1);
-        $manager->persist($reqEmp2);
+        $manager->persist($reqEmp12);
 
-        /* third record */
-        $reqEmp3 = new RequestEmployee();
-        $reqEmp3->setIsOwner(true);
-        $reqEmp3->setRequest($req2);
-        $reqEmp3->setEmployee($emp2);
+        // record three
+        $reqEmp13 = new RequestEmployee();
+        $reqEmp13->setRequest($req1);
+        $reqEmp13->setEmployee($emp3);
 
-        $manager->persist($req2);
-        $manager->persist($emp2);
-        $manager->persist($reqEmp3);
+        $manager->persist($reqEmp13);
+
+        /* second request */
+        // record one
+        $reqEmp21 = new RequestEmployee();
+        $reqEmp21->setIsOwner(true);
+        $reqEmp21->setRequest($req2);
+        $reqEmp21->setEmployee($emp2);
+
+        $manager->persist($reqEmp21);
+
+        // record two
+        $reqEmp22 = new RequestEmployee();
+        $reqEmp22->setRequest($req2);
+        $reqEmp22->setEmployee($emp5);
+
+        $manager->persist($reqEmp22);
+
+        /* third request */
+        // record one
+        $reqEmp31 = new RequestEmployee();
+        $reqEmp31->setIsOwner(true);
+        $reqEmp31->setRequest($req3);
+        $reqEmp31->setEmployee($emp8);
+
+        $manager->persist($reqEmp31);
 
         $manager->flush();
     }
