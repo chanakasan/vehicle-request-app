694dd7b4b31b9cdb44fc0bc76f4a365105ce9cc5
dream89 <chanakasan@gmail.com>
2013-08-06 10:42:58 +0530

    Update admin layout, list view for dataTable for bootstrap2

--- web/app/js/request/list.js
+++ web/app/js/request/list.js
@@ -3,11 +3,18 @@ $(document).ready(function () {
 
     function dataTableInit() {
         $('#requests_list').dataTable({
-            "sDom": "<'row dt-info'<'col-lg-10 col-offset-1'i>>" +
-                "<'row dt-search'<'col-lg-3 col-offset-8'f>>" +
-                "<'row dt-size'<'col-lg-10 col-offset-1'l>>" +
-                "<'row dt-table'<'col-lg-10 col-offset-1't>>" +
-                "<'row dt-pagination'<'col-lg-10 col-offset-1'p>>",
+/* bootstrap 3 */
+//            "sDom": "<'row dt-info'<'col-lg-10 col-offset-1'i>>" +
+//                "<'row dt-search'<'col-lg-3 col-offset-8'f>>" +
+//                "<'row dt-size'<'col-lg-10 col-offset-1'l>>" +
+//                "<'row dt-table'<'col-lg-10 col-offset-1't>>" +
+//                "<'row dt-pagination'<'col-lg-10 col-offset-1'p>>",
+/* bootstrap 2 */
+            "sDom": "<'row dt-info'<'span10 offset1'i>>" +
+                "<'row dt-search'<'span4 offset7'f>>" +
+                "<'row dt-size'<'span10 offset1'l>>" +
+                "<'row dt-table'<'span10 offset1't>>" +
+                "<'row dt-pagination'<'span10 offset1'p>>",
 
             "sPaginationType": "bootstrap",
             "oLanguage": {
