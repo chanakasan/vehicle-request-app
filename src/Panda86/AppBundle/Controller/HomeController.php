dbdd02dbe41ca0c1642cd52ef64c42f33db59aa7
Chanaka Sandaruwan <chanakasan@gmail.com>
2013-08-07 19:57:45 +0530

    Update admin-login css template, Add Dashboard link to main nav

--- src/Panda86/AppBundle/Controller/HomeController.php
+++ src/Panda86/AppBundle/Controller/HomeController.php
@@ -8,6 +8,6 @@ class HomeController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('Panda86AppBundle:Home:index.html.twig');
+        return $this->render('Panda86AppBundle:Dashboard:index.html.twig');
     }
 }
