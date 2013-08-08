a8b6613e45eb0e4ea1a294f84f19c32da751c8dd
Chanaka Sandaruwan <chanakasan@gmail.com>
2013-08-08 11:01:21 +0530

    Add create-view template

--- src/Panda86/AppBundle/Controller/RequestController.php
+++ src/Panda86/AppBundle/Controller/RequestController.php
@@ -233,7 +233,7 @@ class RequestController extends Controller
      *
      * @param mixed $id The entity id
      *
-     * @return Symfony\Component\Form\Form The form
+//     * @return Symfony\Component\Form\Form The form
      */
     private function createDeleteForm($id)
     {
