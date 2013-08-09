0fe7714e518d4806b522f754f10ec4d0c31da088
dream89 <chanakasan@gmail.com>
2013-08-09 12:59:45 +0530

    Add bootstrap modal

--- web/app/js/dashboard/calendar.js
+++ web/app/js/dashboard/calendar.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    
+
     $('#calendar').fullCalendar({
         theme: true,
         header: {
@@ -7,10 +7,19 @@ $(document).ready(function () {
             center: 'title',
             right: 'month,agendaWeek,agendaDay'
         },
+        allDayDefault: false,
         draggable: false,
         editable: false,
-        events: '/approve/a'
+        events: '/approve/a',
+        eventClick: function(calEvent, jsEvent) {
+            jsEvent.preventDefault();
+            $('#myModal').modal('show');
+            // change the border color just for fun
+            $(this).css('border-color', 'red')
+            return false;
+        }
 
     });
+
 
 });
