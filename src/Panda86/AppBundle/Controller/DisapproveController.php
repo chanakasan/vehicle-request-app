b7298c6cf3695debbcc91abd2e827f3120f4d644
dream89 <chanakasan@gmail.com>
2013-08-13 11:22:18 +0530

    Update Disapprove controller and view files

---
+++ src/Panda86/AppBundle/Controller/DisapproveController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Panda86\AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Panda86\AppBundle\Entity\DisapprovedRequest;
+use Panda86\AppBundle\Form\DisapprovedRequestType;
+
+/**
+ * DisapprovedRequest controller.
+ *
+ */
+class DisapproveController extends Controller
+{
+    /**
+     * Creates a new DisapprovedRequest entity.
+     *
+     */
+    public function createAction(Request $request)
+    {
+        $entity  = new DisapprovedRequest();
+        $form = $this->createForm(new DisapprovedRequestType(), $entity);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('request_list', array('id' => $entity->getId())));
+        }
+
+        return $this->render('Panda86AppBundle:Disapprove:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to create a new DisapprovedRequest entity.
+     *
+     */
+    public function newAction()
+    {
+        $entity = new DisapprovedRequest();
+        $form   = $this->createForm(new DisapprovedRequestType(), $entity);
+
+        return $this->render('Panda86AppBundle:Disapprove:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+}
