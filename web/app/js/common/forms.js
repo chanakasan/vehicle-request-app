2cae7f47c361b0b9679b86391d5df40c324b87c5
dream89 <chanakasan@gmail.com>
2013-08-13 15:50:30 +0530

    Set req id using javascript

--- web/app/js/common/forms.js
+++ web/app/js/common/forms.js
@@ -1,11 +1,37 @@
 $(document).ready(function(){
+    function getReqIdFromUrl()
+    {
+        var pathArray = window.location.pathname.split( '/' );
+        pathArray.reverse();
+        return pathArray[0];
+    }
+
+    function setRequestId(id)
+    {
+        $('#req_id_field select').val(id);
+
+    }
+
     $('#approve_btn').click(function(){
         var r = confirm('Do you really want to approve this request, an email confirmation message will be sent to the requester?');
         if (r === true)
         {
+            setRequestId(getReqIdFromUrl());
             return true;
         }
         else
             return false;
     });
+    $('#disapprove_btn').click(function(){
+        var r = confirm('Do you really want to disapprove this request, an email confirmation message will be sent to the requester?');
+        if (r === true)
+        {
+            setRequestId(getReqIdFromUrl());
+            return true;
+        }
+        else
+            return false;
+    });
+
+
 });
