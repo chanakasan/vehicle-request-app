fc9102959a50648ebdc1a0a941e6734316ce238a
Chanaka Sandaruwan <chanakasan@gmail.com>
2013-08-17 20:13:13 +0530

    Add security ROLE_SUPER_ADMIN, Add super admin fixture

--- src/Panda86/UserBundle/DataFixtures/ORM/LoadUserData.php
+++ src/Panda86/UserBundle/DataFixtures/ORM/LoadUserData.php
@@ -13,16 +13,28 @@ class LoadUserData implements FixtureInterface
      */
     public function load(ObjectManager $manager)
     {
-        $userAdmin = new User();
-        $userAdmin->addRole('ROLE_ADMIN');
-        $userAdmin->setEnabled(true);
-        $userAdmin->setFirstName('John');
-        $userAdmin->setLastName('Doe');
-        $userAdmin->setUsername('admin123');
-        $userAdmin->setEmail('admin@webm.com');
-        $userAdmin->setPlainPassword('pass123');
+        $admin = new User();
+        $admin->setEnabled(true);
+        $admin->addRole('ROLE_ADMIN'); # assign admin role
+        $admin->setFirstName('John');
+        $admin->setLastName('Doe');
+        $admin->setUsername('admin123');
+        $admin->setEmail('admin@webm.com');
+        $admin->setPlainPassword('pass123');
 
-        $manager->persist($userAdmin);
+        $manager->persist($admin);
+ 
+        $super_admin = new User();
+        $super_admin->setEnabled(true);
+        $super_admin->setSuperAdmin(true); # create super admin
+        $super_admin->setFirstName('Martin');
+        $super_admin->setLastName('Doe');
+        $super_admin->setUsername('su123');
+        $super_admin->setEmail('su@webm.com');
+        $super_admin->setPlainPassword('pass123');
+
+        $manager->persist($super_admin);
+ 
         $manager->flush();
     }
 }
