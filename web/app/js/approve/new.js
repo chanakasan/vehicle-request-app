8da6344023a9fc40d9a766706d31a83cc325359a
dream89 <chanakasan@gmail.com>
2013-08-26 17:15:46 +0530

    Add radio select for assign vehicle

---
+++ web/app/js/approve/new.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    // set default journey_type to single and hide return time
+    $(function(){
+        $('#vehicle_company').prop('checked', true);
+        $('#div_cab_service').hide();
+    });
+
+
+
+
+});
