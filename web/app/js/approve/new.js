00cd059947b5bfd96ecde173141b533747ab0f8b
dream89 <chanakasan@gmail.com>
2013-08-27 13:28:49 +0530

    Show/hide company vehicle, cab details

--- web/app/js/approve/new.js
+++ web/app/js/approve/new.js
@@ -1,12 +1,35 @@
 $(document).ready(function () {
 
-    // set default journey_type to single and hide return time
+    // By default, select company vehicle and hide cab details
     $(function(){
         $('#vehicle_company').prop('checked', true);
         $('#div_cab_service').hide();
     });
 
+    $('.assign_vehicle').mousedown(function() {
+        $(this).trigger("change");
+    });
 
+    //hide cab detalis
+    $("#vehicle_company").change(function(e){
+        e.preventDefault();
+        var is_checked = $(this).is(':checked');
+        if(is_checked === true)
+        {
+            $('#div_cab_service').hide('slow');
+            $('#div_company_vehicle').show('slow');
+        }
+    });
+    // hide company vehicle details
+    $("#vehicle_cab").change(function(e){
+        e.preventDefault();
+        var is_checked = $(this).is(':checked');
+        if(is_checked === true)
+        {
+            $('#div_company_vehicle').hide('slow');
+            $('#div_cab_service').show('slow');
+        }
+    });
 
 
 });
