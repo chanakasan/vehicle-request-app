4d464bde051e9675ad42613d98d7855cf786517a
dream89 <chanakasan@gmail.com>
2013-09-17 10:50:10 +0530

    Add newController.js using angular

--- web/app/js/request/newController.js
+++ web/app/js/request/newController.js
@@ -8,12 +8,6 @@ angular.element(document).ready(function() {
 angular.module('vrmApp', []);
 
 /* Controllers */
-function formController($scope) {
-
-    $scope.no_days = 1;
-
-    $scope.submitForm = function () {
-        $scope.startForm.submit();
-    }
+function newController($scope) {
 
 }
